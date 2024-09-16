d450b971400546efc2e83b31d662820ab7fc34ec
jakeia <azbazb011@gmail.com>
2024-09-16 22:48:27 +0800

    update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -90,18 +90,6 @@ return {
     },
   },
   {
-    "folke/zen-mode.nvim",
-    cmd = "ZenMode",
-    opts = {
-      plugins = {
-        gitsigns = true,
-        tmux = true,
-        kitty = { enabled = false, font = "+2" },
-      },
-    },
-    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-  },
-  {
     "HiPhish/rainbow-delimiters.nvim",
     event = "VeryLazy",
   },
