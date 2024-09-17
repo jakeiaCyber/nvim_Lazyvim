80fa60a69fed0d7d72f0e53e852c869bbf0a59c0
jakeia <azbazb011@gmail.com>
2024-09-17 15:58:39 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -28,4 +28,3 @@ vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
-vim.opt.signcolumn = "no"
