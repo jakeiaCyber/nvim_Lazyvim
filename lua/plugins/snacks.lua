7ad1696eb633e7e9eef2e8d341040341e9b7ccc8
jakeia <azbazb011@gmail.com>
2024-11-21 00:11:38 +0800

    update

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,7 +7,7 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    notifier = { enabled = true },
+    notifier = { enabled = true, timeout = 1000 },
     quickfile = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
