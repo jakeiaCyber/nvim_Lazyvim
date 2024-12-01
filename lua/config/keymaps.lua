2e69d4a358850a8095933277facc99d6023fa9c0
jakeia <azbazb011@gmail.com>
2024-12-01 16:56:35 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
 vim.keymap.set("n", "<S-h>", "^")
 vim.keymap.set("n", "<S-l>", "$")
 vim.keymap.set("n", ";", ":")
+vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", { silent = true })
